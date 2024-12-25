29b095e252e417ef1a035c68ef9ae5ec73a8a971
phamductinh <phamductinh.t18@gmail.com>
2024-12-26 02:20:41 +0700

    update

--- src/components/Tab5.jsx
+++ src/components/Tab5.jsx
@@ -1,43 +1,83 @@
+import { useState } from "react";
 import LeftSide3 from "./LeftSide3";
 import "./Tab5.scss";
+import LoginForm from "./LoginForm";
+
 function Tab5({ onBack }) {
+	const [isShowModal, setIsShowModal] = useState(false);
+
+	const handleShowForm = () => {
+		setIsShowModal(true);
+	};
+
+	const handleCloseForm = () => {
+		setIsShowModal(false);
+	};
+
 	return (
-		<div className="tab5">
-			<LeftSide3 />
-			<div className="right">
-				<div className="top">
-					<div className="number">
-						<b>5/5</b>
-						Complete sign-up
+		<>
+			<div className="tab5">
+				<LeftSide3 />
+				<div className="right">
+					<div className="top">
+						<div className="number">
+							<b>5/5</b>
+							Complete sign-up
+						</div>
 					</div>
-				</div>
-				<div className="list-content">
-					<h3>
-						{`We're almost finished,`}
-						<p>just a few last details</p>
-					</h3>
-					<div className="list-items">
-						<div className="input-box">
-							<label htmlFor="#">Email *</label>
-							<input type="text" />
+					<div className="list-content">
+						<h3>
+							{`We're almost finished,`}
+							<p>just a few last details</p>
+						</h3>
+						<div className="list-items">
+							{/* <form onSubmit={handleSubmit}>
+								<div className="input-box">
+									<label>Email *</label>
+									<input
+										type="text"
+										value={email}
+										onChange={(e) =>
+											setEmail(e.target.value)
+										}
+									/>
+									{errors.email && (
+										<span className="error">
+											{errors.email}
+										</span>
+									)}
+								</div>
+								<div className="input-box">
+									<label>Password *</label>
+									<input
+										type="password"
+										value={password}
+										onChange={(e) =>
+											setPassword(e.target.value)
+										}
+									/>
+									{errors.password && (
+										<span className="error">
+											{errors.password}
+										</span>
+									)}
+								</div>
+							</form> */}
+							<button type="button" onClick={handleShowForm}>
+								<i className="fa-brands fa-facebook"></i>
+								Continue with Facebook
+							</button>
 						</div>
-						<div className="input-box">
-							<label htmlFor="#">Password *</label>
-							<input type="password" />
-						</div>
-						<button>
-							<i className="fa-brands fa-facebook"></i>Login with
-							Facebook
+					</div>
+					<div className="bottom">
+						<button onClick={onBack} className="back">
+							Back
 						</button>
 					</div>
 				</div>
-				<div className="bottom">
-					<button onClick={onBack} className="back">
-						Back
-					</button>
-				</div>
 			</div>
-		</div>
+			{isShowModal && <LoginForm onClose={handleCloseForm} />}
+		</>
 	);
 }
 
